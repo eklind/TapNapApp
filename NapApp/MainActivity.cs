2412116cac6ed09b1dfc780523936c88e21f023d
JonathanKarlsson92 <jonathan.karlsson13@gmail.com>
2018-01-31 19:24:49 -0500

    changed stuff

--- NapApp/MainActivity.cs
+++ NapApp/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Widget;
 using System;
 using Android.Media;
 
+using System.Diagnostics;
+
 namespace NapApp
 {
     [Activity(Label = "NapApp", MainLauncher = true, Icon = "@mipmap/icon")]
@@ -15,8 +17,10 @@ namespace NapApp
         private TextView _touchTimerTextView;
         private ImageView _touchMeImageView;
         MediaPlayer _player;
+        Stopwatch elapTime = new Stopwatch();
 
         public int timer = 0;
+        public int sleepTime = 20;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -27,38 +31,43 @@ namespace NapApp
 
             _touchMeImageView.Touch += TouchMeImageViewOnTouch;
             _player = MediaPlayer.Create(this, Resource.Raw.alarm);
+            //elapTime.Reset();
         }
 
         private void TouchMeImageViewOnTouch(object sender, View.TouchEventArgs touchEventArgs)
         {
             string message;
+            
+            if (elapTime.ElapsedMilliseconds < sleepTime * 1000)
+            {
+                elapTime.Reset();
+                _player.Start();
+            }
+            
             switch (touchEventArgs.Event.Action & MotionEventActions.Mask)
             {
+                //Happens one time
                 case MotionEventActions.Down:
                     message = "Program running";
                     timer = timer + 1;
                     break;
+                //Happens many times
                 case MotionEventActions.Move:
                     // Handle both the Down and Move actions.
                     message = "Program running";
                     timer =timer+1;
                     break;
-
+                //Happens one time
                 case MotionEventActions.Up:
-                    message = "Wake up!";
+                    elapTime.Start();
+                    message = "Wake up in!"+(elapTime.ElapsedMilliseconds); //milliseconds
                     timer = 0;
-                    _player.Start();
                     break;
-
+                //Never
                 default:
                     message = "default";
                     break;
             }
-            /*if (timer > 100)
-            {
-                _player.Reset();
-                _player.Start();
-            }*/
             _touchInfoTextView.Text = message;
             _touchTimerTextView.Text = timer.ToString();
         }
